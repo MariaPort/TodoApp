d29c658a960473c8d8ed3acaebe9f6c17b1e7007
Mary <mportnova13@gmail.com>
2019-02-18 17:39:16 +0400

    :fire: :fire:

---
+++ public/precache-manifest.a90c79fb7cb4633c5d3904be25f90585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64e7daa1a4b37ffa3b68",
+    "url": "/static/js/main.64e7daa1.chunk.js"
+  },
+  {
+    "revision": "6356b65c008adadcb202",
+    "url": "/static/js/1.6356b65c.chunk.js"
+  },
+  {
+    "revision": "64e7daa1a4b37ffa3b68",
+    "url": "/static/css/main.8c0ed8f0.chunk.css"
+  },
+  {
+    "revision": "fd8e019d3a935477eb297f4d14b3277f",
+    "url": "/index.html"
+  }
+];
